ab409fcde72d2f92ebbe1433e5d4f9b7d813d31a
DanielSanchez052 <70668399+DanielSanchez052@users.noreply.github.com>
2024-12-16 20:42:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { splitVendorChunkPlugin } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    origin: 'https://addycoin.online',
+    origin: 'https://moneymanage.my',
     headers: {
       'Strict-Transport-Security': 'max-age=2592000; includeSubDomains', // Adds HSTS options to your website, with a expiry time of 1 day
       'X-Content-Type-Options': 'nosniff', // Protects from improper scripts runnings
@@ -18,7 +18,7 @@ export default defineConfig({
       'Permissions-Policy':
         'accelerometer=(), autoplay=(), camera=(), encrypted-media=(), microphone=(), geolocation=(), gyroscope=(), magnetometer=(), midi=(), payment=(), picture-in-picture=(), sync-xhr=(self), usb=()',
       'Content-Security-Policy':
-        "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com ; img-src 'self'; frame-src 'self'; connect-src 'self' https://dev-moneymanage-identity.addycoin.online https://dev-moneymanage.addycoin.online wss://dev-moneymanage.addycoin.online",
+        "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com ; img-src 'self'; frame-src 'self'; connect-src 'self' https://dev-moneymanage-identity.moneymanage.my https://dev-moneymanage.moneymanage.my wss://dev-moneymanage.moneymanage.my",
     },
   },
   //https://fonts.googleapis.com https://mnw9y2xpf0.execute-api.us-east-1.amazonaws.com https://dev-moneymanage.originalcoin.online
